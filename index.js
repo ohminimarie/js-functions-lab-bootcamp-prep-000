7e7dba32de9da1312a7bf3c4684d94a36884f10f
Marie Alcantara <mcjalcantara@gmail.com>
2017-09-17 00:39:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`;
-}
-
-function holidayCountdown(holiday, days) {
-  return `it's ${days} days until ${holiday}!`;
-}
+// function happyHolidaysTo(holiday, name) {
+//   return `Happy ${holiday}, ${name}!`;
+// }
+// 
+// function holidayCountdown(holiday, days) {
+//   return `it's ${days} days until ${holiday}!`;
+// }
